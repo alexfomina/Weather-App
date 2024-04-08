db1e8ebb579cd818296f7b7b6e208676f6940d74
Alexandra Fomina <fomina@chapman.edu>
2024-04-08 12:56:14 -0700

    update

--- setupTests.js
+++ setupTests.js
@@ -1,25 +1,3 @@
 const { setup } = require('jest-environment-jsdom-sixteen');
 
 setup();
-
-// Mock TextEncoder
-global.TextEncoder = require('util').TextEncoder;
-
-// Polyfill TextDecoder
-global.TextDecoder = require('util').TextDecoder;
-
-const { JSDOM } = require('jsdom');
-
-// Create a DOM environment
-const dom = new JSDOM('<!doctype html><html><body></body></html>', {
-  url: 'https://example.com', // Set a URL to mimic a complete browser environment
-});
-
-// Assign the window and document objects to the global object
-global.window = dom.window;
-global.document = dom.window.document;
-
-// Assign navigator object to the global object
-global.navigator = {
-  userAgent: 'node.js',
-};

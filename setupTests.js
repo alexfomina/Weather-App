6c6fc18ac3213d865f4230f4843c223e08a7daf0
Alexandra Fomina <fomina@chapman.edu>
2024-04-08 11:57:16 -0700

    update setup

--- setupTests.js
+++ setupTests.js
@@ -21,13 +21,14 @@ global.window = dom.window;
 global.document = dom.window.document;
 
 // Mock localStorage
-const localStorageMock = {
-  getItem: jest.fn(),
-  setItem: jest.fn(),
-  removeItem: jest.fn(),
-  clear: jest.fn(),
-};
-global.localStorage = localStorageMock;
+Object.defineProperty(global, 'localStorage', {
+  value: {
+    getItem: jest.fn(),
+    setItem: jest.fn(),
+    removeItem: jest.fn(),
+    clear: jest.fn(),
+  },
+});
 
 // Assign navigator object to the global object
 global.navigator = {

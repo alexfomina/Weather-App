90aa1e437a2a30c8bc474b68b846f9a7c921a0a5
Alexandra Fomina <fomina@chapman.edu>
2024-04-08 01:03:53 -0700

    update file

--- setupTests.js
+++ setupTests.js
@@ -4,12 +4,12 @@ const { JSDOM } = require('jsdom');
 // Mock TextEncoder
 global.TextEncoder = require('util').TextEncoder;
 
+// Polyfill TextDecoder
+global.TextDecoder = require('util').TextDecoder;
+
 const dom = new JSDOM('<!doctype html><html><body></body></html>', {
   url: 'https://example.com', // Set a URL to mimic a complete browser environment
-  pretendToBeVisual: true, // Pretend to be a visual browser, enabling certain features like scrollIntoView
-  storageQuota: 10000000, // Set a larger storage quota if needed
 });
-
 global.document = dom.window.document;
 global.window = dom.window;
 global.navigator = dom.window.navigator;

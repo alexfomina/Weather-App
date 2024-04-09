2892e827699f2a99d8c5bcd68bd5f62ef1baf8c2
Alexandra Fomina <fomina@chapman.edu>
2024-04-09 16:00:27 -0700

    updated eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  languageOptions: {
+    globals: {
+      document: true,
+      console: true,
+      fetch: true,
+      test: true,
+      expect: true
+    }
+  }
+};
+
+
+
